7170e9588ed1d8ca08d72d404999887af8dca89c
Ashok Koda <a.koda59@gmail.com>
2021-12-14 17:10:04 +0530

    UC1:Validate firstname and first letter should be capital

--- src/main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -1,11 +1,33 @@
 package com.bridgelabz.userregistration;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+
+	// Validate first name
+	public static void validFirstName(String firstName) {
+		Pattern pattern = Pattern.compile("^[A-Z]{1}+[a-z]{3,}");
+		Matcher matcher = pattern.matcher(firstName);
+
+		if (!matcher.find()) {
+			System.out.println("Firstname letter should be capital.");
+		} else {
+			System.out.println("Firstname is valid.");
+		}
+	}
 
 	public static void main(String[] args) {
 
 		System.out.println("----------Welcome To User Registration------------");
 
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter Firstname: ");
+		String fname = sc.next();
+		System.out.println("Firstname is: " + fname);
+		validFirstName(fname);
 	}
 
 }

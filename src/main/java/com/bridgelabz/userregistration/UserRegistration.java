c7cd5fe6a688a7a1a49548c4223273d75d706cd2
Ashok Koda <a.koda59@gmail.com>
2021-12-14 17:03:29 +0530

    Welcome to user registration

--- src/main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -1,30 +1,10 @@
 package com.bridgelabz.userregistration;
 
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class UserRegistration {
-	
-	//Validate first name
-	public static void validFirstName(String firstName) {
-		Pattern pattern = Pattern.compile("^[A-Z]{1}+[a-z]{3,}");
-		Matcher matcher = pattern.matcher(firstName);
-		
-		if(!matcher.find()) {
-			System.out.println("Firstname should be capital.");
-		}else {
-			System.out.println("Firstname is valid.");
-		}
-	}
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter Firstname: ");
-		String fname = sc.next();
-		System.out.println("Firstname is: " + fname);
-		validFirstName(fname);
+
+		System.out.println("----------Welcome To User Registration------------");
 
 	}
 
